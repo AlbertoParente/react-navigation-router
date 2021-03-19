f85bf9737c45558375af6ededa62ffb92a59eaaa
AlbertoParente <albertoparente20@gmail.com>
2021-03-18 22:06:33 -0300

    Creating project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import ReactDom from 'react-dom'
 import React from 'react'
+import App from './view/App'
 
 ReactDom.render(
-    <h1>App</h1>,
+    <App />,
     document.getElementById('root')
 )
